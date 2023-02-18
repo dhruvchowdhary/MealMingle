41fb4360f41b45fd46fd8c3e8caa4414bbcfc7ed
Dhruv Chowdhary <dhruvchowdhary@berkeley.edu>
2023-02-18 15:05:50 -0800

    flip camera

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const video = document.getElementById('video');
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
+const flipButton = document.getElementById('flip-button');
 
 // Use HTML Media Capture API to access the camera
 navigator.mediaDevices.getUserMedia({ video: true })
@@ -10,6 +11,35 @@ navigator.mediaDevices.getUserMedia({ video: true })
   .catch(error => {
     console.error(error);
   });
+
+// Flip the camera when the button is clicked
+flipButton.addEventListener('click', () => {
+  // Get a list of available video input devices
+  navigator.mediaDevices.enumerateDevices()
+    .then(devices => {
+      // Find the video input device that is not the current device
+      const currentDeviceId = video.srcObject.getVideoTracks()[0].getSettings().deviceId;
+      const nextDevice = devices.find(device => device.kind === 'videoinput' && device.deviceId !== currentDeviceId);
+      if (nextDevice) {
+        // Use the next device to create a new video stream
+        const constraints = {
+          video: {
+            deviceId: { exact: nextDevice.deviceId }
+          }
+        };
+        navigator.mediaDevices.getUserMedia(constraints)
+          .then(stream => {
+            video.srcObject = stream;
+          })
+          .catch(error => {
+            console.error(error);
+          });
+      }
+    })
+    .catch(error => {
+      console.error(error);
+    });
+});
 
 // Capture a frame from the video stream at 1 second intervals
 setInterval(() => {
