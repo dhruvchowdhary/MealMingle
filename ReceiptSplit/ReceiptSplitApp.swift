0e254593d96404a64fbd742d42ad71aa7fcec436
Dhruv Chowdhary <dhruvchowdhary@berkeley.edu>
2023-02-18 13:03:19 -0800

    Initial Commit

---
+++ ReceiptSplit/ReceiptSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReceiptSplitApp.swift
+//  ReceiptSplit
+//
+//  Created by Dhruv Chowdhary on 2/18/23.
+//
+
+import SwiftUI
+
+@main
+struct ReceiptSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
