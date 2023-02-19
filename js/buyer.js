81485fef1919459d458634dda2036c7a1c963895
Hanson Leung <53881772+hanson-leung@users.noreply.github.com>
2023-02-19 06:15:43 -0800

    test

---
+++ js/buyer.js
@@ -0,0 +1,14 @@
+$(window).on("load", function () {
+  $(".modal").hide();
+});
+
+$(".close").on("click", function () {
+  $(".modal").hide();
+});
+
+let openShareModal = function () {
+  $("#share").show();
+};
+let openSettleModal = function () {
+  $("#settle").show();
+};
